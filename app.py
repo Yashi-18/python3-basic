b3037dc0ba4b7668e89ddb143c366d4608c98631
Petani kode <siyashidyzaa04@gmail.com>
2025-09-01 21:23:42 +0700

    tambahan skrip

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ print("BELAJAR DASAR GIT")
 # git add [nama file yg berubah]
 # git commit -m "isi pesan perubahan"
 # git push -u origin main
+#---carara nambah app.py file---
+#git add app.py
+# git commit -m "peeannys"
+# git push
